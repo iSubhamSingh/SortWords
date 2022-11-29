98b793525dcbc4d10eabff22a4a92cd0f5556e1e
Subham Singh <singh_subham@outlook.com>
2022-11-29 23:11:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
-function sortWords() {
-let string = document.getElementById("words").value;
+function sort(){
+let string = document.getElementById("string").value;
+
 let words = string.split(' ');
 
 words.sort();
 
 let str = "";
 for(let i = 0; i < words.length; i++){
-    str += words[i];
+    str += words[i]+ " ";
+}
+document.getElementById("result").value = str;
 }
 
-document.getElementById("result").innerHTML = str;
+function eraseText(){
+    document.getElementById("string").value = "";
+    document.getElementById("result").value = "";
 }
